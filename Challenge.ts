f5f52f39a8835881ef799ec9253f51d345a12332
Sbabalwe Roux <156612216+SbaRou339@users.noreply.github.com>
2025-11-27 13:05:07 +0200

    Add example usage for minimumCarsNeeded function

--- Challenge.ts
+++ Challenge.ts
@@ -22,3 +22,7 @@ function minimumCarsNeeded(P: number[], S: number[]) {
 
 // Example usage:
 console.log(minimumCarsNeeded([1, 4, 1], [1, 5, 1]))
+
+console.log(minimumCarsNeeded([4, 4, 2, 4], [5, 5, 2, 5]))
+
+console.log(minimumCarsNeeded([2, 3, 4, 2], [2, 5, 7, 2]))
